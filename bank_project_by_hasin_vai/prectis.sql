2d68374a0daa60e66beb4d683fbc05f2c85dd38a
Masricozzaman Anas <153457604+MasriconzzamanAnas@users.noreply.github.com>
2024-10-26 02:01:36 +0600

    mani

--- bank_project_by_hasin_vai/prectis.sql
+++ bank_project_by_hasin_vai/prectis.sql
@@ -3,3 +3,10 @@
 --- count customar by GROUP BY city
 SELECT COUNT(*) AS TOTAL, city, postal_code FROM customer GROUP BY CITY, `POSTAL_CODE`
 
+
+--- Work With Multiple Where Clause
+SELECT * FROM customer WHERE (CITY = 'Salem' AND `POSTAL_CODE` IN ('03079','03082')) OR (CITY = 'Wilmington' AND `POSTAL_CODE` IN ('01887','01888'))
+
+--- Data from multiple tables according to different conditions
+SELECT customer.cust_id, individual.first_name, individual.last_name, customer.postal_code, customer.address FROM customer, individual WHERE city = 'salem' AND customer.CUST_ID = individual.CUST_ID
+
